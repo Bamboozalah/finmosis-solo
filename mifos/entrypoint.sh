7d9c914660ab179809c15f666d9e102945a88b53
Bamboozalah <49986836+Bamboozalah@users.noreply.github.com>
2019-11-25 10:24:59 -0600

    entrypoint.sh ssl attempt 1

--- mifos/entrypoint.sh
+++ mifos/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat -dname "CN=tomcat" -keystore /usr/share/tomcat.keystore
+mkdir -p /opt/certbot
+wget https://dl.eff.org/certbot-auto -O /opt/certbot/certbot-auto
+chmod a+x /opt/certbot/certbot-auto
+/opt/certbot/certbot-auto certonly --debug --non-interactive --email devin.wright@neocova.com --agree-tos --standalone -d finmosis.net --keep-until-expiring
+openssl pkcs12 -export -in /etc/letsencrypt/live/finmosis.net/cert.pem -inkey /etc/letsencrypt/live/finmosis.net/privkey.pem -out /usr/share/tomcat -name tomcat -CAfile /etc/letsencrypt/live/finmosis.net/chain.pem -caname root -passout pass:password
 
-catalina.sh run
+catalina.sh run 
